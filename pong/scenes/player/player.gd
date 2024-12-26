831cdefd69cfb6b137bb436703652d7e4c2230b2
Hérick Bittencourt <professional.herickvvb@gmail.com>
2024-12-26 20:50:32 -0300

    Fix: Player X axis lock and StaticBody class

--- pong/scenes/player/player.gd
+++ pong/scenes/player/player.gd
@@ -1,17 +1,23 @@
-extends CharacterBody2D
+class_name Player
+extends StaticBody2D
 
 @export var move_speed = 255
 @export var player_id = 0
+var initial_position
+
+func _ready() -> void:
+	initial_position = global_position
+
+func _process(delta: float) -> void:
+	global_position.x = initial_position.x
 
 func _physics_process(delta: float) -> void:
 	if player_id == 0: return
-	var move = false
+	
+	var move_vector = Vector2(0,0)
 	if Input.is_action_pressed("P%s_UP" % player_id):
-		# self.position.y -= move_speed * delta
-		self.velocity.y = -move_speed
-		move = true
+		move_vector = Vector2(0, -move_speed * delta)
 	if Input.is_action_pressed("P%s_DOWN" % player_id):
-		# self.position.y += move_speed * delta
-		self.velocity.y = move_speed
-		move = true
-	if move: move_and_slide()
+		move_vector = Vector2(0, move_speed * delta)
+	
+	if move_vector != Vector2.ZERO: move_and_collide(move_vector)
