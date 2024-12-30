2e656f455576ace9d6574a0fe9566a374d9e887a
Hérick Bittencourt <professional.herickvvb@gmail.com>
2024-12-30 18:52:11 -0300

    Feat: Added player and level
    - Created player scene, contains sprite, box collision and sideways movement using A and D
    - Created level scene, contains world boundaries to prevent the player or ball from escaping the camera

---
+++ breakout/scenes/player/player.gd
@@ -0,0 +1,8 @@
+extends CharacterBody2D
+
+@export var move_speed = 5
+
+func _physics_process(delta: float) -> void:
+	var move = Input.get_vector("p1_left","p1_right","EMPTY","EMPTY") * move_speed
+	
+	move_and_collide(move)
