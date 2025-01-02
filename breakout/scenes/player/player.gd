6c2719f2b690ce227b39ceb722842c5c3e7647a8
Hérick Bittencourt <professional.herickvvb@gmail.com>
2025-01-02 19:51:08 -0300

    Fix: Ternary Operator Type Casting

--- breakout/scenes/player/player.gd
+++ breakout/scenes/player/player.gd
@@ -11,7 +11,7 @@ func _physics_process(delta: float) -> void:
 	var move = (
 			Input.get_vector("p1_left","p1_right","EMPTY","EMPTY") * 
 			move_speed * 
-			(.5 if int(Input.is_action_pressed("p1_slow")) else 1 )
+			(.5 if Input.is_action_pressed("p1_slow") else 1.0 )
 		)
 	
 	move_and_collide(move)
