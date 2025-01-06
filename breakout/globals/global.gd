a56ebf4b3810f4c3ca729b83c4a3c45b9d6cb3a2
Hérick Bittencourt <professional.herickvvb@gmail.com>
2025-01-06 19:36:36 -0300

    Feat: Fullscreen toggle
    - You can now toggle fullscreen by pressing F11

--- breakout/globals/global.gd
+++ breakout/globals/global.gd
@@ -21,3 +21,7 @@ func get_level_node():
 
 func get_level_tilemap():
 	return get_level_node().get_node("TileMap")
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("fullscreen_toggle"):
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN if DisplayServer.window_get_mode() != DisplayServer.WINDOW_MODE_FULLSCREEN else DisplayServer.WINDOW_MODE_MAXIMIZED)
