c37a188e73f780bab2e9fba1fb52b44f201c4aae
Hérick Bittencourt <professional.herickvvb@gmail.com>
2025-01-10 22:04:10 -0300

    Feat: Snake movement and WIP sprite update
    - Added snake movement logic
    - Tick speed is no longer exported due to an iniatialization crash when modified at the start (Timer instance does not exist during the setter's trigger)
    - Implementing sprite update for snake (Currently facing issues with the curved sprite direction)

--- snake/src/scenes/player/player.gd
+++ snake/src/scenes/player/player.gd
@@ -4,6 +4,7 @@ extends TileMapLayer
 ## The player is a tilemap responsible for controlling the snake's movement
 ## and functionality
 
+@export_category("Snake Properties")
 ## Size of the snake
 @export var size = 3:
 	set(value):
@@ -14,24 +15,29 @@ extends TileMapLayer
 		tick_speed = 1 - clamp(size * 0.15, .1, .75)
 
 ## Interval in seconds in which the snake moves
-@export var tick_speed: float = 1:
+var tick_speed: float = .25:
 	set(value):
 		tick_speed = value
 		$Tick.start(value)
 
 ## Tiles occupied by the snake, first index is tail, last one is head
-var snake_tiles = [
-	Vector2i(-2,0),
-	Vector2i(-1,0),
-	Vector2i(0,0),
-]
+#var snake_tiles = [
+	#Vector2i(-2,0),
+	#Vector2i(-1,0),
+	#Vector2i(0,0),
+#]
 
+var snake_tiles = [
+	{'pos': Vector2i(-2,0), 'rot': Vector2i(0,0)},
+	{'pos': Vector2i(-1,0), 'rot': Vector2i(0,0)},
+	{'pos': Vector2i(0,0), 'rot': Vector2i(0,0)},
+]
 ## Snake's head direction, defines where
 ## it will go in the next tick
 var snake_direction: Vector2i = Vector2i(1,0):
 	set(value):
 		# Snake can't turn 180 degrees
-		if snake_tiles[snake_tiles.size() - 1] + value == snake_tiles[snake_tiles.size() - 2]:
+		if snake_tiles[snake_tiles.size() - 1].pos + value == snake_tiles[snake_tiles.size() - 2].pos:
 			return
 		
 		# Snake can't move diagonally, nor can it stop
@@ -47,6 +53,32 @@ func _ready() -> void:
 	$Tick.start(tick_speed)
 
 func _on_tick_timeout() -> void:
-	pass
-	# TODO: Implement snake_tiles movement logic
 	# TODO: Implement snake sprite update
+	
+	snake_tiles.append({
+		'pos': snake_tiles[snake_tiles.size() - 1].pos + snake_direction,
+		'rot': snake_direction
+	})
+	
+	set_cell(snake_tiles.pop_front().pos)
+	
+	for i in range(snake_tiles.size()):
+		var atlas_coords = Vector2i(0,0)
+		if i == 0:
+			atlas_coords.y = 1
+		else:
+			if i < snake_tiles.size()-1:
+				if snake_tiles[i].rot != snake_tiles[i+1].rot:
+					atlas_coords.y = 0
+				else:
+					atlas_coords.y = 2
+			
+		if i == snake_tiles.size() - 1:
+			atlas_coords.y = 3
+		
+		atlas_coords.x = abs(
+			(snake_tiles[i].rot.x + snake_tiles[i].rot.y * 2 + 1)
+			 % 4
+			)
+		set_cell(snake_tiles[i].pos, 0, atlas_coords)
+	
