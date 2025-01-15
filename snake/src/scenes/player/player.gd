be7cca089ac53a95db48a379597ef4f4e5865e45
Hérick Bittencourt <professional.herickvvb@gmail.com>
2025-01-15 14:20:34 -0300

    Feat: Snake size updates

--- snake/src/scenes/player/player.gd
+++ snake/src/scenes/player/player.gd
@@ -84,7 +84,8 @@ func _on_tick_timeout() -> void:
 		'rot': snake_direction
 	})
 	
-	set_cell(snake_tiles.pop_front().pos)
+	if snake_tiles.size() >= size:
+		set_cell(snake_tiles.pop_front().pos)
 	
 	for i in range(snake_tiles.size()):
 		var current_tile: Dictionary = snake_tiles[i]
