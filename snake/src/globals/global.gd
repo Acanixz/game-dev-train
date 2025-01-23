4587f8145bc1de385c117c1b91c4efe5f0c78523
Hérick Bittencourt <professional.herickvvb@gmail.com>
2025-01-23 10:40:30 -0300

    Feat: Load scene method

--- snake/src/globals/global.gd
+++ snake/src/globals/global.gd
@@ -1,4 +1,13 @@
 extends Node
 
-func get_level():
+const SCENES: Array[String] = [
+	"res://scenes/main/main.tscn",
+	"res://scenes/level/level.tscn",
+]
+
+func get_level() -> Node:
 	return get_tree().root.get_node("Level")
+	
+func load_scene(id: int = 0):
+	var target_scene: int = id if id > 0 and id < SCENES.size() else 0
+	get_tree().change_scene_to_file(SCENES[target_scene])
