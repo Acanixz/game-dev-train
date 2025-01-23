ab104cbdba5fc8d2a97c6de9230fb3a2a108e321
Hérick Bittencourt <professional.herickvvb@gmail.com>
2025-01-23 15:37:45 -0300

    Feat: Fullscreen toggle

--- snake/src/globals/global.gd
+++ snake/src/globals/global.gd
@@ -11,3 +11,7 @@ func get_level() -> Node:
 func load_scene(id: int = 0):
 	var target_scene: int = id if id > 0 and id < SCENES.size() else 0
 	get_tree().change_scene_to_file(SCENES[target_scene])
+
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("fullscreen_toggle"):
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN if DisplayServer.window_get_mode() != DisplayServer.WINDOW_MODE_FULLSCREEN else DisplayServer.WINDOW_MODE_MAXIMIZED)
