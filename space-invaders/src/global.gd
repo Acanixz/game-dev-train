318840dfae66bfbaee87abe14c1e2559d92172a8
Hérick Bittencourt <professional.herickvvb@gmail.com>
2025-01-29 11:06:14 -0300

    Fix: Fullscreen toggle handle
    This commit does not affect any issues
    
    - Fullscreen toggle is now properly handled according to documentation
    https://docs.godotengine.org/en/stable/classes/class_node.html#class-node-private-method-input

--- space-invaders/src/global.gd
+++ space-invaders/src/global.gd
@@ -1,5 +1,6 @@
 extends Node
 
 func _input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("fullscreen_toggle"):
+	if event.is_action_pressed("fullscreen_toggle"):
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN if DisplayServer.window_get_mode() != DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN else DisplayServer.WINDOW_MODE_MAXIMIZED)
+		get_viewport().set_input_as_handled()
