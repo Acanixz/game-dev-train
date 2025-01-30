627f3f7e114c9bf2c11113ba24da67bb0eb448b8
Hérick Bittencourt <professional.herickvvb@gmail.com>
2025-01-30 09:43:16 -0300

    Fix: Projectile position issues
    This issue affects #19
    
    - Fixed a bug that caused projectiles get positioned incorrectly due to local positioning instead of global

--- space-invaders/src/scripts/invader.gd
+++ space-invaders/src/scripts/invader.gd
@@ -8,7 +8,7 @@ const POP_EFFECT: PackedScene = preload("res://scenes/effects/pop_effect.tscn")
 
 func shoot():
 	var projectile: EnemyProjectile = ENEMY_PROJECTILE.instantiate()
-	projectile.position = Vector2(position.x, position.y + 10)
+	projectile.global_position = Vector2(global_position.x, global_position.y + 10)
 	get_node("/root/Level/Projectiles").call_deferred("add_child", projectile)
 	
 func _on_died():
