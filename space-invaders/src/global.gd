5099070afba3449c4ed5413a3555a5c312ba89ba
Hérick Bittencourt <professional.herickvvb@gmail.com>
2025-01-31 13:44:19 -0300

    Fix: Input during paused scene
    This commit affects no issues
    
    - global.gd can no longer be paused, fixes the fullscreen toggle not responding during death sequence

--- space-invaders/src/global.gd
+++ space-invaders/src/global.gd
@@ -1,5 +1,8 @@
 extends Node
 
+func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("fullscreen_toggle"):
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN if DisplayServer.window_get_mode() != DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN else DisplayServer.WINDOW_MODE_MAXIMIZED)
