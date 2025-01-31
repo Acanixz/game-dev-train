ee5b5ab7a49af46fea70f7056d810ef740d7575d
Hérick Bittencourt <professional.herickvvb@gmail.com>
2025-01-31 17:50:24 -0300

    Feat: Scoring Structure and Save/Load Data
    This commit affects #32 and #30
    
    - Added scoring and high-score mechanics within global.gd
    - Implemented auto-save/load
    
    Note: There isn't a way to get a score yet, a power outage disrupted development today and corrupted some of the files. Nothing was lost, except time.

--- space-invaders/src/global.gd
+++ space-invaders/src/global.gd
@@ -1,7 +1,24 @@
 extends Node
 
+var current_score: int = 0
+var high_score_structure: Dictionary = {
+		"score": 0,
+		"name": "???",
+	}
+var high_scores: Array
+
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS
+	
+	for i in range(5):
+		high_scores.append(high_score_structure.duplicate())
+	
+	load_game()
+	
+## Auto-save
+func _notification(what):
+	if what == NOTIFICATION_WM_CLOSE_REQUEST:
+		save_game()
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("fullscreen_toggle"):
@@ -14,3 +31,53 @@ func random_pitch_and_play(sound: AudioStreamPlayer, base_pitch: float = 1):
 	sound.pitch_scale = base_pitch + (randf_range(-0.05, 0.05))
 	sound.play()
 	
+## Resets the current score
+func reset_score() -> void:
+	current_score = 0
+	
+## Checks for high-score, then stores if it is
+func store_score(username: String) -> void:
+	# Check for high-score
+	var position: int = -1
+	for i in range(high_scores.size()):
+		var value: Dictionary = high_scores[i]
+		if current_score > value.score:
+			position = i
+			break
+
+	# Is high-score?
+	if position != -1:
+		# Store data into structure
+		var score_data: Dictionary = high_score_structure.duplicate()
+		score_data.name = username
+		score_data.score = current_score
+		
+		# Insert data and remove additional scores
+		high_scores.insert(position, score_data)
+		if high_scores.size() > 5:
+			high_scores.pop_back()
+		
+	reset_score()
+
+func add_score(increment) -> void:
+	current_score += increment
+
+func save_game() -> void:
+	print("Saving game..")
+	var save_dict: String = JSON.stringify({
+		"high_scores": high_scores
+	})
+	FileAccess.open("user://space-invaders.save", FileAccess.WRITE).store_line(save_dict)
+	print("Game saved")
+	
+func load_game() -> void:
+	if not (FileAccess.file_exists("user://space-invaders.save")): return
+	var save_data: String = FileAccess.open("user://space-invaders.save", FileAccess.READ).get_line()
+	var parsed_saved = JSON.parse_string(save_data)
+	
+	print("Save found, loading..")
+	
+	# SAVE DATA
+	high_scores = parsed_saved.high_scores
+	
+	print("Game loaded")
